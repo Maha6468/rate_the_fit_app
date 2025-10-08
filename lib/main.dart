5c32a070d65c9ae4280a956e6d8c27a08a17c83e
Maha6468 <motherma7633@gmail.com>
2025-10-09 01:48:16 +0600

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rate_the_fit_app/screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,15 +8,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:
+      home:Splash_Screen(),
     );
   }
 }
---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Splash_Screen extends StatelessWidget {
+  const Splash_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Container(
+        height:MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.black,Color(0xFF3B2F2F)],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight
+          )
+        ),
+
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  shape: BoxShape.circle,
+                  //border: Border.all(color: Colors.red),
+                  boxShadow: [
+                  BoxShadow(
+                  color: Colors.black54,
+                  blurRadius: 15,
+                  spreadRadius: 2,
+                  offset: Offset(0, 5),
+                ),
+          ],
+                ),
+                child:Image.asset("assets/images/maha.png",height: 100,width: 100,fit:BoxFit.cover,)
+              ),
+              SizedBox(height: 30,),
+              Text("Rate The Fit App",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17,color: Colors.amber.shade50),),
+
+            ],
+          ),
+        ),
+      ),
+
+
+    );
+  }
+}
