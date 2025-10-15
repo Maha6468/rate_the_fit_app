b9e21deecb6a0158105dd1aaa843cf222f1c1c13
Maha6468 <motherma7633@gmail.com>
2025-10-15 20:14:22 +0600

    last update

--- lib/screen/home_flow/home.dart
+++ lib/screen/home_flow/home.dart
@@ -66,8 +66,7 @@ class _HomeState extends State<Home> {
                     child: ListView.builder(
                       scrollDirection: Axis.horizontal,
                       padding: EdgeInsets.symmetric(horizontal: 10),
-
-                      itemCount: userImage.length+1,
+                      itemCount: userImage.length + 1,
                       itemBuilder: (context, index) {
                         if (index == 0) {
                           return Padding(
@@ -96,8 +95,7 @@ class _HomeState extends State<Home> {
                               ],
                             ),
                           );
-                        }
-                        else {
+                        } else {
                           final img = userImage[index - 1];
                           final name = userNames[index - 1];
                           return buildStory(img, name);
@@ -106,10 +104,8 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   const SizedBox(height: 10),
-
                   buildPostCard(context),
                   buildPostCard(context)
-
                 ],
               ),
             ),
@@ -133,18 +129,17 @@ Widget buildStory(String imgPath, String name) {
   );
 }
 
-
-
 Widget buildPostCard(BuildContext context) {
-  final width = MediaQuery.of(context).size.width;
 
   return Container(
-    width: width,
-    margin: const EdgeInsets.all(10),
+    height: MediaQuery.of(context).size.height,
+    width: MediaQuery.of(context).size.width,
     decoration: BoxDecoration(
-      color: const Color(0xFF1E1E1E),
-      borderRadius: BorderRadius.circular(15),
-    ),
+        gradient: LinearGradient(
+            colors: [Colors.black, Color(0xFF3B2F2F)],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight)),
+
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -153,26 +148,31 @@ Widget buildPostCard(BuildContext context) {
             backgroundImage: AssetImage('assets/images/firad.JPG'),
           ),
           title: const Text("Cameron Williamson",
-              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
           subtitle: const Text("7529 E. Pecan St.",
               style: TextStyle(color: Colors.grey)),
           trailing: Container(
-            padding: const EdgeInsets.all(6),
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 1),
             decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(10),
+              color: Color(0xFF3B2F2F),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(-5),
+                topRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(-5),
+              ),
             ),
-            child: TextButton(onPressed: (){
-
-  },
-                child: Text("Style Type", style: TextStyle(color: Colors.white70)),
-          ),),),
+            child: TextButton(
+              onPressed: () {},
+              child: Text("Style Type", style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ),
         ClipRRect(
           borderRadius: BorderRadius.circular(10),
           child: Image.asset(
             "assets/images/firad.JPG",
-            width: width,
-            height: width * 0.6,
             fit: BoxFit.fill,
           ),
         ),
@@ -184,7 +184,8 @@ Widget buildPostCard(BuildContext context) {
             direction: Axis.horizontal,
             allowHalfRating: true,
             itemSize: 25,
-            itemBuilder: (context, _) => const Icon(Icons.star, color: Colors.amber),
+            itemBuilder: (context, _) =>
+                const Icon(Icons.star, color: Colors.amber),
             onRatingUpdate: (rating) {},
           ),
         ),
